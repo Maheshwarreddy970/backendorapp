47e6fb695e3b263986c10c24ed37f11f0411e8bb
Maheshwarreddy970 <mr8382455@gmail.com>
2026-07-21 23:05:40 +0530

    ading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { VisitorTracker } from "@/components/admin/storefront/visitor-tracker";
-
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,8 +45,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body suppressHydrationWarning className="h-full bg-white text-gray-900">
-        <VisitorTracker />
         {children}
+        <Analytics />
       </body>
     </html>
   );
